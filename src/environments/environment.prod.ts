f222a459b12d8637e10acb16de0b3bf33d7552f4
Tu Nombre <tu.email@ejemplo.com>
2026-05-09 13:48:19 -0400

    api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://TU_DOMINIO/api', // Reemplazar con URL de producción
-  googleClientId: 'YOUR_GOOGLE_CLIENT_ID_HERE',
+  apiUrl: 'https://backend-app-beb.onrender.com/api', // Reemplazar con URL de producción
+  googleClientId: '1057417146171-d5ih4k9gggjuvqtaoqpoj8govc6nvk6r.apps.googleusercontent.com',
 };
